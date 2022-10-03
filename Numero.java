6e3fbe26b39021abe23545a1f44120fa385fa712
Matheus Valentim <114266242+NegaoValente@users.noreply.github.com>
2022-10-03 13:42:50 -0300

    Add files via upload

---
+++ Numero.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+         
+        Scanner numero= new Scanner(System.in);
+       
+        
+        System.out.print("Informe um número:");
+        float numeral = numero.nextFloat();
+
+        System.out.print("O número informado foi:" +numeral);
+         
+        
+    
+    }
+}
